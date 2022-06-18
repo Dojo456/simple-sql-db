dd505f15af5dc1b17f16098cfaa29772e58d6f78
Daniel Liao <yazhengliao@gmail.com>
2022-06-18 18:38:39 -0400

    Initial commit

---
+++ backend/backend.go
@@ -0,0 +1,13 @@
+// The backend package interacts with storage devices to provide the data storage functionality of the
+// database.
+package backend
+
+// Put puts the value at the specified key
+func Put(key string, value interface{}) error {
+	return nil
+}
+
+// Get gets the value at the specified key
+func Get(key string) (interface{}, error) {
+	return nil, nil
+}
---
+++ frontend/frontend.go
@@ -0,0 +1,8 @@
+package frontend
+
+type command string
+
+const (
+	PutCommand command = "PUT"
+	GetCommand command = "GET"
+)

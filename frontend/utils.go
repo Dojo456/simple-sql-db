ec1a366475334bb0181d17816e4dfb4603aaf932
Daniel Liao <yazhengliao@gmail.com>
2022-06-18 20:03:48 -0400

    Run configs for IntelliJ

---
+++ frontend/utils.go
@@ -0,0 +1,9 @@
+package frontend
+
+import "regexp"
+
+func isEmptyString(s string) bool {
+	val, _ := regexp.Match(`\S`, []byte(s))
+
+	return !val
+}
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Dojo456/simple-inmem-db/frontend"
+)
 
 func main() {
-	fmt.Println("hello world")
+	var input string
+	fmt.Println("enter command:")
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		return
+	}
+
+	cmd, err := frontend.Parse(input)
+
+	fmt.Println(cmd)
+	fmt.Println(err)
 }
